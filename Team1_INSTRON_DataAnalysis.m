48602ccbe32d04b95ea797031d06bce50d36c9f5
fefoglino <42367586+fefoglino@users.noreply.github.com>
2023-11-21 02:30:00 -0500

    test

--- Team1_INSTRON_DataAnalysis.m
+++ Team1_INSTRON_DataAnalysis.m
@@ -34,13 +34,62 @@ for k = 1:length(materials)
     end
 end
 
-%% Display Data:
+%% Linearize data, do the math
+% I made this separate bc i kept getting weird index errors with the skeleton code
+% and it was easier to debug like this
 for k = 1:length(materials)
-    figure(k);
-
     for n = 1:length(teamnames)
         Force = cell2mat(data.(materials{k}).Force{1,n});
         Pos  = cell2mat(data.(materials{k}).Pos{1,n});
+
+        % zero the data
+        Force = Force - Force(1);
+        Pos = Pos - Pos(1);
+
+        % get rid of trailing NaN values only if they are present
+        idx = find(isnan(Force), 1, 'first');
+        if idx>0
+            Force = Force(1:idx-1);
+            Pos = Pos(1:idx-1);
+        end
+
+        % get rid of non-linear data only if max value comes before last value
+        idx2 = find(Force == max(Force), 1, 'first');
+        if idx2>0
+            Force = Force(1:idx2-1);
+            Pos = Pos(1:idx2-1);
+        end
+        % save the final versions of the force and pos arrays
+        linearized.(materials{k}).Force{1,n} = Force;
+        linearized.(materials{k}).Pos{1,n} = Pos;
+
+        % find stiffness
+        p = polyfit(Pos, Force, 1);
+        linearized.(materials{k}).Stiffness(1,n) = p(1);
+
+        % find stress
+        A = cell2mat(data.(materials{k}).Area(1,n));
+        linearized.(materials{k}).Stress{1,n} = Force./A;
+
+        % find strain
+        L0 = cell2mat(data.(materials{k}).L0(1,n));
+        linearized.(materials{k}).Strain{1,n} = Pos./L0;
+
+    end
+end
+
+%% Display Data:
+
+% Position and Force graphs
+figure(1);
+hold on
+for k = 1:length(materials)
+    subplot(2,2,k);
+
+    for n = 1:length(teamnames)
+        Force = linearized.(materials{k}).Force{1,n};
+        Pos  = linearized.(materials{k}).Pos{1,n};
+
         plot(Pos, Force);
         hold on
         title(strcat("Force_vs_Position_(", materials{k}, ")"), 'Interpreter', 'None');
@@ -48,6 +97,25 @@ for k = 1:length(materials)
         ylabel('Force (N)');
     end
     legend(teamnames, 'Location', 'Best');
+end
+hold off
+
+% Stress and Strain graphs
+figure(2);
+hold on
+for k = 1:length(materials)
+    subplot(2,2,k);
+
+    for n = 1:length(teamnames)
+        Stress = linearized.(materials{k}).Stress{1,n};
+        Strain = linearized.(materials{k}).Strain{1,n};
+
+        plot(Strain, Stress);
+        hold on
+        title(strcat("Stress vs. Strain (", materials{k},")"), 'Interpreter', 'None');
+        xlabel("Strain");
+        ylabel("Stress");
+    end
 end
 
 %% Useful Commands:
@@ -57,37 +125,42 @@ end
     % gradient() --> computes the derivative of a dataset
 
 %% EXAMPLE OF SKIN_RAW PARTIAL ANALYSIS:
-    figure(7); clf;
-
-    for n = 1:length(teamnames)
-        Force = cell2mat(data.(materials{3}).Force{1,n});
-        Pos  = cell2mat(data.(materials{3}).Pos{1,n});
-
-        % Zero force/position data
-        Force = Force - Force(1);
-        Pos = Pos - Pos(1);
-        
-        % Determine if and where NaN entries exist in force/position data
-        idx = find(isnan(Force), 1, 'first');
-        Force = Force(1:idx-1);
-        Pos = Pos(1:idx-1);
-    
-        % Crop off all values outside of linear range (You need to figure
-        % this out! :))
-        idx2 = find(Force == max(Force), 1, 'first');
-        Force_linear = Force(:);
-        Pos_linear = Pos(:);
-
-        % Calculate linear regression of force/position data to determine
-        % stiffness
-        p = polyfit(Pos_linear, Force_linear, 1);
-        Stiffness(1,n) = p(1);
-
-        % Plot force/position data
-        plot(Pos_linear, Force_linear);
-        hold on
-        title(strcat("Force_vs_Position_(", materials{3}, ")"), 'Interpreter', 'None');
-        xlabel('Position (mm)');
-        ylabel('Force (N)');
-    end
-    legend(teamnames, 'Location', 'Best');
+% figure(2); clf;
+% hold on
+% for k = 1:length(materials)
+%     % make a 2x2 subplot where each graph is a dif material
+%     subplot(2,2,k);
+%     for n = 1:length(teamnames)
+%         Force = cell2mat(data.(materials{3}).Force{1,n});
+%         Pos  = cell2mat(data.(materials{3}).Pos{1,n});
+% 
+%         % Zero force/position data
+%         Force = Force - Force(1);
+%         Pos = Pos - Pos(1);
+% 
+%         % Determine if and where NaN entries exist in force/position data
+%         idx = find(isnan(Force), 1, 'first');
+%         Force = Force(1:idx-1);
+%         Pos = Pos(1:idx-1);
+% 
+%         % Crop off all values outside of linear range (You need to figure
+%         % this out! :))
+%         idx2 = find(Force == max(Force), 1, 'first');
+%         Force_linear = Force(1:idx2);
+%         Pos_linear = Pos(1:idx2);
+% 
+%         % Calculate linear regression of force/position data to determine
+%         % stiffness
+%         p = polyfit(Pos_linear, Force_linear, 1);
+%         Stiffness(1,n) = p(1);
+% 
+%         % Plot force/position data
+%         plot(Pos_linear, Force_linear);
+%         hold on
+%         title(strcat("Force_vs_Position_(", materials{3}, ")"), 'Interpreter', 'None');
+%         xlabel('Position (mm)');
+%         ylabel('Force (N)');
+%     end
+%     legend(teamnames, 'Location', 'Best');
+% end
+% hold off
